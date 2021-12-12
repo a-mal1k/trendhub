d49e3e8087f50f9f6721128ec7d40c96caa4e576
Arman Ali <armanalicid@gmail.com>
2021-12-12 18:55:16 +0530

    optimized

--- src/components/movieContainer/MovieInfo.js
+++ src/components/movieContainer/MovieInfo.js
@@ -36,7 +36,7 @@ function MovieInfo() {
                     } else {
                         info = { name: "Sorry couldn't retrieve any data" }
                     }
-                    console.log(info)
+                
                     mov.push(info)
                     setMovieState(mov)
                     console.log(movieState[0])
@@ -45,82 +45,59 @@ function MovieInfo() {
                 console.log("Eror while fetching data")
             })
     }, [movieState,url]);
-   
-    let poster, title, name, tagline, overview, genres, companies, release, background, runtime, status, homepage, rating, startSeries, episodeRuntime
-   
-    movieState.map((info) =>(
-        title = info.title,
-        name = info.name,
-        tagline = info.tagline,
-        overview = info.overview,
-        runtime = info.runtime,
-        episodeRuntime = info.episodeRuntime,
-        status = info.status,
-        homepage = info.homepage,
-        rating = info.rating,
-        startSeries = info.startSeries,
-        genres = info.genres.map((genre) => (
-            genre.name
-        )).join(', '),
-        companies = info.companies.map((company) => (
-            company.name
-        )).join(', '),
-        release = info.release,
-        background = `https://image.tmdb.org/t/p/original${info.background}`,
-        poster = `https://image.tmdb.org/t/p/w500${info.poster}`
-    ))
-    const styles = { width: '100%', backgroundImage: background ? (`url(${background})`) : null }
-    if(movieState.length<1){
+    if(movieState<1){
         return <Fallback />
     } else{
-    return (
-        <div>
-            <section style={styles} className="infoSection ">
+        return (
+       movieState.map((info, id) =>{
+           return (
+        <div key={id}>
+            <section style={{ width: '100%', backgroundImage: `url(${`https://image.tmdb.org/t/p/original${info.background}`})` }} className="infoSection ">
                 <div className="outer-container">
                     <div className="container-box">
                         <div className="infoBox">
                             <div className="poster">
-                                <img src={poster} alt={tagline}/>
+                                <img src={`https://image.tmdb.org/t/p/w500${info.poster}`} alt={info.tagline}/>
                             </div>
                             <div className="info">
-                                <div className="movie-name"><h1>{title}{name}</h1></div>
+                                <div className="movie-name"><h1>{info.title}{info.name}</h1></div>
 
                                 <div className="overview">
-                                    <h2>{tagline}</h2>
-                                    <p>{overview}</p>
+                                    <h2>{info.tagline}</h2>
+                                    <p>{info.overview}</p>
                                 </div>
 
                                 <div className="genres-companies">
-                                    <h2>{genres}</h2>
-                                    <p>{companies}</p>
+                                    <h2>{info.genres.map((genre) => (genre.name)).join(', ')}</h2>
+                                    <p>{info.companies.map((company) => (company.name)).join(', ')}</p>
                                 </div>
 
                                 <div className="detail-container">
                                     <div className="column">
                                         <div className="details">
                                             <h3>Original Release</h3>
-                                            <h2>{release}{startSeries}</h2>
+                                            <h2>{info.release}{info.startSeries}</h2>
                                         </div>
                                         <div className="details">
                                             <h3>Status</h3>
-                                            <h2>{status}</h2>
+                                            <h2>{info.status}</h2>
                                         </div>
                                     </div>
                                     <div className="column">
                                         <div className="details">
                                             <h3>Running Time</h3>
-                                            <h2>{runtime}{episodeRuntime} min</h2>
+                                            <h2>{info.runtime}{info.episodeRuntime} min</h2>
                                         </div>
                                         <div className="details">
                                             <h3>Vote Average</h3>
-                                            <h2>{rating}/10</h2>
+                                            <h2>{info.rating}/10</h2>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="links">
                                     <div className="li"><Link className="link" to="/">Movies</Link></div>
                                     <div className="li"><Link className="link" to="/TvShows" >Tv Shows</Link></div>
-                                    <div className="li"><a className="link" href={homepage} target="_blank" rel="noreferrer">View More Details</a></div>
+                                    <div className="li"><a className="link" href={info.homepage} target="_blank" rel="noreferrer">View More Details</a></div>
                                 </div>
                             </div>
 
@@ -129,8 +106,10 @@ function MovieInfo() {
                 </div>
             </section>
         </div>
-
+           )
+    })
     )
+
     }
 }
 
